744556d31bdc113d2758179b243c6d590ec1cb05
André Wronscki <andrewronscki@gmail.com>
2023-03-23 20:32:23 -0300

    fix: pass healthcheck route in guard

--- src/auth/presenters/guards/permissions.guard.ts
+++ src/auth/presenters/guards/permissions.guard.ts
@@ -5,7 +5,9 @@ import { UnauthorizedError } from '@/shared/domain';
 @Injectable()
 export class PermissionsGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const { headers } = context.switchToHttp().getRequest();
+    const { headers, originalUrl } = context.switchToHttp().getRequest();
+
+    if (originalUrl === '/api/v1/healthcheck') return true;
 
     if (headers.client_id !== process.env.CLIENT_ID)
       throw new UnauthorizedError();
